34b85f42d8231593fca7f2c61e7132459d43055d
Sai65 <sayi65@live.cn>
2019-04-08 18:36:29 +0900

    _id fix

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,64 +1,46 @@
 <template>
-  <v-container>
-    <v-layout flex-child wrap>
-      <v-flex xs12 sm12 md8>
-        <v-layout column justify-center align-center>
-          <v-flex v-for="image in images" :key="image.num">
-            <v-card class="mb-2 mr-2 ml-2" max-width="700">
-              <v-card-title>
-                <v-list-tile class="grow">
-                  <v-list-tile-avatar color="grey darken-3">
-                    <v-img
-                      class="elevation-6"
-                      src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-                    ></v-img>
-                  </v-list-tile-avatar>
-
-                  <v-list-tile-content>
-                    <v-list-tile-title>
-                      <nuxt-link to="/_id">@Evan You</nuxt-link>
-                    </v-list-tile-title>
-                  </v-list-tile-content>
-
-                  <v-layout justify-end>
-                    <v-layout align-end justify-center column fill-height>
-                      <span
-                        class="grey--text text--darken-2 font-weight-light caption"
-                        ><v-icon right small>fas fa-thumbs-up</v-icon
-                        >16,544</span
-                      >
-                      <span
-                        class="pt-2 grey--text text--darken-2 font-weight-light caption"
-                        >1 hours ago</span
-                      >
-                    </v-layout>
-                  </v-layout>
-                </v-list-tile>
-              </v-card-title>
-              <v-divider />
-
-              <v-card-text class="title">
-                "Turns out semicolon-less style is easier and safer in TS
-                because most gotcha edge cases are type invalid as well."
-                <v-spacer />
-              </v-card-text>
-            </v-card>
-          </v-flex>
-        </v-layout>
+  <v-container grid-list-md text-xs-center>
+    <v-layout wrap>
+      <v-flex>
+        <v-card>
+          <v-img
+            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+          ></v-img>
+          <v-divider />
+          <v-card-title>
+            <h3 class="headline mb-0">@Kangaroo</h3>
+          </v-card-title>
+          <v-card-text
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.</v-card-text
+          >
+          <v-divider />
+          <v-btn block flat nuxt="/setting/profile">Change Profile</v-btn>
+          <v-divider />
+          <v-card-actions> </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm12 md4 d-flex>
+        <v-img
+          src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+        ></v-img>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
 <script>
 export default {
   layout: 'default',
   data() {
-    return {
-      pwdShow: false
-    }
-  },
-  methods: {
-    submit() {}
+    return {}
   }
 }
 </script>
+<style>
+.v-content {
+  padding-top: 5px !important;
+}
+</style>
