80e546958b7565773261fb49157a622a69c01509
Sai65 <sayi65@live.cn>
2019-04-09 19:30:52 +0900

    fix _id

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,28 +1,30 @@
 <template>
-  <v-container grid-list-md text-xs-center>
-    <v-layout wrap>
-      <v-flex>
-        <v-card>
-          <v-img
-            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-          ></v-img>
-          <v-divider />
-          <v-card-title>
-            <h3 class="headline mb-0">@Kangaroo</h3>
-          </v-card-title>
-          <v-card-text
-            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.</v-card-text
-          >
-          <v-divider />
-          <v-btn block flat nuxt="/setting/profile">Change Profile</v-btn>
-          <v-divider />
-          <v-card-actions> </v-card-actions>
-        </v-card>
+  <v-container grid-list-xl text-xs-center>
+    <v-layout row wrap fluid>
+      <v-flex d-flex xs12 sm4 md5 lg4 xl3 fluid>
+        <v-layout wrap fluid>
+          <v-card>
+            <v-img
+              src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+            ></v-img>
+            <v-divider />
+            <v-card-title>
+              <h3 class="headline mb-0">@Kangaroo</h3>
+            </v-card-title>
+            <v-card-text
+              >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.</v-card-text
+            >
+            <v-divider />
+            <v-btn block flat>Change Profile</v-btn>
+            <v-divider />
+            <!-- <v-card-actions> </v-card-actions> -->
+          </v-card>
+        </v-layout>
       </v-flex>
-      <v-flex xs12 sm12 md4 d-flex>
+      <v-flex d-flex xs12 sm8 md7 lg8 xl9>
         <v-img
           src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
         ></v-img>
@@ -35,7 +37,25 @@
 export default {
   layout: 'default',
   data() {
-    return {}
+    return {
+      cardHeight: ''
+    }
+  },
+  mounted() {
+    this.onResize()
+  },
+  methods: {
+    onResize() {
+      console.log(this.$vuetify.breakpoint.name)
+      switch (this.$vuetify.breakpoint.name) {
+        case 'lg':
+          this.cardHeight = 'height=600'
+          break
+        case 'xl':
+          this.cardHeight = 'height=800'
+          break
+      }
+    }
   }
 }
 </script>
